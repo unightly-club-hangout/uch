9802aafb15449726879acaef45a62ffb77a2269c
Tells Fox (Silent masonwolf) <silentmason1@gmail.com>
2025-08-27 03:46:13 -0400

    rr

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import AdminDashboard from './AdminDashboard';
+import AdminDashboard from './Dashboard';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
